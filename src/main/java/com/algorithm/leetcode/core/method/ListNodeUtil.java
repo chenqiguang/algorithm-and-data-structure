8761935b2ef408d0631e37238aae859c5b75461f
chenqiguang <045dhby@pl>
2019-09-24 22:25:29 +0800

    fix

--- src/main/java/com/algorithm/leetcode/core/method/ListNodeUtil.java
+++ src/main/java/com/algorithm/leetcode/core/method/ListNodeUtil.java
@@ -1,6 +1,7 @@
 package com.algorithm.leetcode.core.method;
 
 import com.algorithm.leetcode.core.model.ListNode;
+import com.algorithm.leetcode.core.model.TreeNode;
 import org.omg.PortableServer.LIFESPAN_POLICY_ID;
 import org.springframework.stereotype.Component;
 
@@ -442,7 +443,25 @@ public class ListNodeUtil {
         return pre.next;
     }
 
+    /**
+     * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+     * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+     *
+     * 示例:
+     * 给定的有序链表： [-10, -3, 0, 5, 9],
+     * 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+     *       0
+     *      / \
+     *    -3   9
+     *    /   /
+     *  -10  5
+     * @param head
+     * @return
+     */
+    public TreeNode sortedListToBST(ListNode head) {
 
+        return null;
+    }
 
 
 
---
+++ src/main/java/com/algorithm/leetcode/core/model/TreeNode.java
@@ -0,0 +1,20 @@
+package com.algorithm.leetcode.core.model;
+
+/**
+ * 项目名称:  algorithm-and-data-structure
+ * 模块名称:
+ * 说明:
+ * JDK 版本: JDK1.8
+ *
+ * @author 作者：chenqiguang
+ * 创建日期：2019-09-24
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
