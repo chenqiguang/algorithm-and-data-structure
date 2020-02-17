d27d426a993f95b0916f00e1a2be2d3a54ef01f6
chenqiguang <chenqiguang@xforceplus.com>
2020-02-17 11:01:57 +0800

    fix

--- src/main/java/com/algorithm/leetcode/core/method/TreeNodeUtil.java
+++ src/main/java/com/algorithm/leetcode/core/method/TreeNodeUtil.java
@@ -126,7 +126,43 @@ public class TreeNodeUtil {
         return G[n];
     }
 
+    /**
+     * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+     *
+     * 假设一个二叉搜索树具有如下特征：
+     *
+     * 节点的左子树只包含小于当前节点的数。
+     * 节点的右子树只包含大于当前节点的数。
+     * 所有左子树和右子树自身必须也是二叉搜索树。
+     * @param root
+     * @return
+     */
+    public boolean isValidBST(TreeNode root) {
+        return checkBst(root,null,null);
+    }
 
+    private boolean checkBst(TreeNode root,Integer lower,Integer upper){
+        if (root ==null){
+            return true;
+        }
+        int val = root.val;
+        if (lower!=null && val <= lower){
+            return false;
+        }
+
+        if (upper!=null && val >= upper){
+            return false;
+        }
+
+        if (!checkBst(root.right,val,upper)){
+            return false;
+        }
+
+        if (!checkBst(root.left,lower,val)){
+            return false;
+        }
+        return true;
+    }
 
 
 }
--- src/test/java/com/algorithm/leetcode/test/treenode/TreeNodeTest.java
+++ src/test/java/com/algorithm/leetcode/test/treenode/TreeNodeTest.java
@@ -32,8 +32,20 @@ public class TreeNodeTest extends BaseUnitTest {
     @Test
     public void testNumTrees(){
         int i = treeNodeUtil.numTrees(5);
-
         System.out.println(i);
+    }
+
+    @Test
+    public void testisValidBST(){
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(15);
+        root.left.left = null;
+        root.left.right = null;
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(20);
+        boolean validBST = treeNodeUtil.isValidBST(root);
+        System.out.println("testisValidBST : "+validBST);
     }
 
 
